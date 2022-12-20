a0fa68091063c045d16477d70e913b0affca5f3d
srivz <srivathsananim@gmail.com>
2022-12-20 13:58:05 +0530

    new file:   public/_redirects 	modified:   public/index.html 	deleted:    public/logo192.png 	deleted:    public/logo512.png 	deleted:    src/App.css 	modified:   src/App.js 	new file:   src/assets/css/style.css 	new file:   src/assets/images/Group 3.svg 	new file:   src/assets/images/Group 8.png 	new file:   src/assets/images/Icon awesome-google.svg 	new file:   src/assets/images/Icon feather-check.svg 	new file:   src/assets/images/Rectangle 4.svg 	new file:   src/assets/js/script.js 	new file:   src/components/LoginHome.js 	new file:   src/components/Manager/HomeBlock.js 	new file:   src/components/Manager/HomeList.js 	new file:   src/components/Navs/NavBar.js 	new file:   src/components/TeamMate/Home.js 	deleted:    src/index.css 	modified:   src/index.js 	deleted:    src/logo.svg 	deleted:    src/reportWebVitals.js 	deleted:    src/setupTests.js

---
+++ src/assets/js/script.js
@@ -0,0 +1,12 @@
+function myFunction() {
+    var count = 0
+    if (count === 0){
+        document.querySelector(".action-box").style.display = "block";
+        count += 1
+    }
+    else{
+        document.querySelector(".action-box").style.display = "none";
+        count = 0
+    }
+
+  }
