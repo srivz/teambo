1a90f82ac0374872f3f06d4d5726812e713bd5c9
Basu-Dev-Ghosh <basu1735@gmail.com>
2022-12-23 15:17:36 +0530

    Signup and login pages(Some improvements)

--- src/components/LoginPages/Signup.js
+++ src/components/LoginPages/Signup.js
@@ -1,10 +1,11 @@
 import React from 'react'
 import logo from "../../assets/images/Group 3.svg";
+
 export default function Signup() {
     return (
-        <div id="main text-center">
-            <div className="container mt-5 ">
-                <div className="form-box1 ">
+
+        <div className="container mt-5 login-container">
+            <div className="form-box1">
                     <div className="img text-center">
                         <img className="w-50" src={logo} alt />
                     </div>
@@ -13,11 +14,11 @@ export default function Signup() {
                         <div className="form-group mb-4 ">
                             <div className="row">
                                 <div className="col-sm-6 col-md-6">
-                                    <label htmlFor="name" className="fw-bold"> Name</label>
-                                    <input className="form-control rounded-3" id="name" type="name" name="name " placeholder="name" />
+                                <label htmlFor="name">Name</label>
+                                <input className="form-control rounded-3" id="name" type="name" name="name " placeholder="Name" />
                                 </div>
                                 <div className="col-sm-6 col-md-6">
-                                    <label htmlFor="email" className="fw-bold">Email</label>
+                                <label htmlFor="email">Email</label>
                                     <input className="form-control" id="email" type="email" name="Email " placeholder="Email" />
                                 </div>
                             </div>
@@ -25,17 +26,19 @@ export default function Signup() {
                         <div className="form-group mb-4">
                             <div className="row">
                                 <div className="col-sm-6 col-md-6">
-                                    <label htmlFor="pwd" className="fw-bold">Company Name</label>
+                                <label htmlFor="pwd">Company Name</label>
                                     <select className="form-select" aria-label="Default select example">
-                                        <option selected>Select Company</option>
+                                    <option selected hidden>Select Company</option>
                                         <option value={1}>The Madras branding Company</option>
                                         <option value={2}>Brand Moustache</option>
                                         <option value={3}>Little Show</option>
                                         <option value={4}>Facebook</option>
+                                    <option value={4} className="add-new-button"><button>Add new</button></option>
+
                                     </select>
                                 </div>
                                 <div className="col-sm-6 col-md-6">
-                                    <label htmlFor="pwd" className="fw-bold">Password:</label>
+                                <label htmlFor="pwd">Password:</label>
                                     <input type="password" className="form-control" id="pwd" placeholder="Password" />
                                 </div>
                             </div>
@@ -43,16 +46,16 @@ export default function Signup() {
                         <div className="form-group mb-4">
                             <div className="row">
                                 <div className="col-sm-6 col-md-6">
-                                    <label htmlFor="pwd" className="fw-bold">Designation</label>
+                                <label htmlFor="pwd">Designation</label>
                                     <select className="form-select" aria-label="Default select example">
-                                        <option selected>Select Designation</option>
+                                    <option selected hidden>Select Designation</option>
                                         <option value={1}>One</option>
                                         <option value={2}>Two</option>
                                         <option value={3}>Three</option>
                                     </select>
                                 </div>
                                 <div className="col-sm-6 col-md-6">
-                                    <label htmlFor="pwd" className="fw-bold">Re-enter Password:</label>
+                                <label htmlFor="pwd">Re-enter Password:</label>
                                     <input type="password" className="form-control" id="pwd" placeholder="Re-enter Password" />
                                 </div>
                             </div>
@@ -61,7 +64,7 @@ export default function Signup() {
                     </form>
                 </div>
             </div>
-        </div>
+
 
     )
 }
