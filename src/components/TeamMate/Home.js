ad313b2061b95134eb6a8cac9b72c941f59ec008
Basu-Dev-Ghosh <basu1735@gmail.com>
2023-01-07 19:19:34 +0530

    Teammate problem fixed

--- src/components/TeamMate/Home.js
+++ src/components/TeamMate/Home.js
@@ -72,6 +72,7 @@ export default function Home() {
           startTimeInMs: timeInMs,
         })
       } else if (task.updates[task.updates.length - 1].status === 'On Going') {
+        // alert(length)
         pauseTask(e, i, length)
       }
     })
@@ -118,7 +119,7 @@ export default function Home() {
     }
     var timeGapInMs = totTime
     var timeGap = getHourFormatFromMilliSeconds(totTime)
-    update(ref(db, `teammate/${id}/tasks/${index}/updates/${length - 1}`), {
+    update(ref(db, `teammate/${id}/tasks/${index}/updates/${teammate.tasks[index].updates.length - 1}`), {
       status: 'Paused',
       totalTime: timeGap,
       totalTimeInMs: timeGapInMs,
@@ -408,9 +409,9 @@ export default function Home() {
                                   .filter((info) => {
                                     return filter !== 'All'
                                       ? info.updates[info.updates.length - 1]
-                                        .status === filter
+                                        ?.status === filter
                                       : info.updates[info.updates.length - 1]
-                                        .status !== filter
+                                        ?.status !== filter
                                   })
                                   .map((info, index) => {
                                     return (
