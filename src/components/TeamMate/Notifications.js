59fda99fe5ac7ce0abb9f0e25aa83c1c1107b351
srivz <srivathsananim@gmail.com>
2023-01-10 08:39:59 +0530

    modified:   src/components/TeamMate/Notifications.js

--- src/components/TeamMate/Notifications.js
+++ src/components/TeamMate/Notifications.js
@@ -1,5 +1,5 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { onValue, ref, remove, set, update } from 'firebase/database'
+import { onValue, ref, remove, set } from 'firebase/database'
 import React, { useState } from 'react'
 import {
     Badge,
