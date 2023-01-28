9394d6de4b836678efad2721d550b7cd5482765a
srivz <srivathsananim@gmail.com>
2023-01-28 11:18:51 +0530

    modified:   src/components/Manager/Attendance.js

--- src/components/Manager/Attendance.js
+++ src/components/Manager/Attendance.js
@@ -76,7 +76,11 @@ function ShowModal(props) {
                 size="md"
                 aria-labelledby="contained-modal-title-vcenter"
             >
-                <Modal.Header closeButton> </Modal.Header>
+                <Modal.Header closeButton>
+                    <Modal.Title id="contained-modal-title-vcenter">
+                        Attendance Sheet
+                    </Modal.Title>
+                </Modal.Header>
                 <Modal.Body>
                     {props?.attendance !== null ?
                     <Table>
@@ -149,9 +153,6 @@ export default function Attendance(props) {
                 centered
             >
                 <Modal.Header closeButton>
-                    <Modal.Title id="contained-modal-title-vcenter">
-                        Attendance Sheet
-                    </Modal.Title>
                 </Modal.Header>
                 <Modal.Body style={{ position: 'relative' }} >
                     <Row><Col className='text-end'>
